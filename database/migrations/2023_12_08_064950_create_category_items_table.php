e71d93382f65f935bdff56fdeeae00f2651c3373
anbuxplor <anbu.ayyakannu@xplortechnologies.com>
2023-12-08 12:35:44 +0530

    Database migration files created  for Items, Category and Mapping table of Category and Items.

---
+++ database/migrations/2023_12_08_064950_create_category_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade'); //  onDelete(cascade) - rows deleted in a child table when corresponding rows are deleted in the parent table.
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_items');
+    }
+};
